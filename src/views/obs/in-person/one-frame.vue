eb8ed24729a5e7e2d20f21043683147ec27d528a
Leping08 <dereks008@gmail.com>
2022-11-29 18:45:02 -0500

    Comment out blue border

--- src/views/obs/in-person/one-frame.vue
+++ src/views/obs/in-person/one-frame.vue
@@ -1,85 +1,9 @@
 <template>
   <outline>
     <div class="flex justify-around items-center h-screen">
-      <div class="relative w-4/5 border-4 border-primary rounded-lg">
-        <!-- <div
-          class="
-            absolute
-            w-32
-            -mt-48
-            top-0
-            right-64
-            py-2
-            rounded-t-lg
-            bg-white
-            border-t-4 border-l-4 border-r-4 border-primary
-            text-lg text-gray-600 text-center
-          "
-        >
-          Michael
-        </div>
-        <div
-          class="
-            absolute
-            w-32
-            -mt-48
-            top-0
-            left-64
-            py-2
-            rounded-t-lg
-            bg-white
-            border-t-4 border-l-4 border-r-4 border-primary
-            text-lg text-gray-600 text-center
-          "
-        >
-          Derek
-        </div> -->
-
-        <!-- <div
-          class="
-            absolute
-            w-32
-            -mt-48
-            -bottom-12
-            right-32
-            py-2
-            rounded-b-lg
-            bg-white
-            border-b-4
-            border-l-4
-            border-r-4
-            border-primary
-            text-lg
-            text-gray-600
-            text-center
-          "
-        >
-          Derek
-        </div>
-        <div
-          class="
-            absolute
-            w-32
-            -mt-48
-            -bottom-12
-            left-32
-            py-2
-            rounded-b-lg
-            bg-white
-            border-b-4
-            border-l-4
-            border-r-4
-            border-primary
-            text-lg
-            text-gray-600
-            text-center
-          "
-        >
-          Michael
-        </div> -->
-
-        <div class="aspect-video"></div>
-      </div>
+      <!-- <div class="relative w-4/5 border-4 border-primary rounded-lg">
+        <div class="aspect-video h-screen"></div>
+      </div> -->
     </div>
   </outline>
 </template>
