d75d36635483b8850169bc74559ef6e73f98d4c3
Leping08 <dereks008@gmail.com>
2022-12-10 21:09:32 -0500

    Remove cache busting

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// Used for cache-busting
-const hash = Math.floor(Math.random() * 90000) + 10000;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -13,14 +10,5 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  build: { // update the cache-busting hash
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name]` + hash + `.js`,
-        chunkFileNames: `[name]` + hash + `.js`,
-        assetFileNames: `[name]` + hash + `.[ext]`
-      }
-    }
   }
 });
