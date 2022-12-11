68de789c0554dcfc8ba0bf4827c26f91d28b3c7e
Leping08 <dereks008@gmail.com>
2022-12-10 20:59:23 -0500

    Fix the cache problem

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// Used for cache-busting
+const hash = Math.floor(Math.random() * 90000) + 10000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -11,4 +14,13 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: { // update the cache-busting hash
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
+  }
 });
