eb52119c48439d401e5f32a2bf5044be2eef42f4
Leping08 <dereks008@gmail.com>
2024-12-26 22:40:13 -0500

    Fix logout error

--- src/views/auth/logout.vue
+++ src/views/auth/logout.vue
@@ -7,10 +7,11 @@ import { onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/auth.js";
 
+const router = useRouter();
+
 onMounted(async () => {
   const authStore = useAuthStore();
   await authStore.logout();
-  const router = useRouter();
   router.push({ name: "auth.login" });
 });
 </script>
